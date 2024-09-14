d965b2ae484cbad332667444af83f5094c791970
Raymond <86973702+RaymondCao001@users.noreply.github.com>
2024-09-14 23:49:38 +1000

    Fianlly working version after 5 hour try....

---
+++ frontend/src/Payment.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Payment() {
+    return (
+        <div>
+            <h1>Payment Page</h1>
+            <p>This is the payment page for our food ordering system.</p>
+        </div>
+    );
+}
+
+export default Payment;
