45a74fa124533db59b6f52bdc195b709b0918b09
Raymond <86973702+RaymondCao001@users.noreply.github.com>
2024-09-15 00:29:58 +1000

    Payment frontend done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,20 +2,18 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
-import Payment from './Payment';  // 导入 Payment 组件
+import Payment from './Payment';
 
 function App() {
     return (
         <Router>
             <div className="App">
                 <header className="App-header">
-                    {/* 导航链接 */}
                     <nav>
                         <Link to="/">Home</Link> | <Link to="/Payment">Payment</Link>
                     </nav>
                 </header>
 
-                {/* 定义路由 */}
                 <Routes>
                     <Route path="/" element={<div>Home Page Content</div>} />
                     <Route path="/Payment" element={<Payment />} />
