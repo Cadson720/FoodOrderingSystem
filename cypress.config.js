de7910634ad764e84869fc2c396d2fcd204c8f74
Raymond <86973702+RaymondCao001@users.noreply.github.com>
2024-09-17 17:04:58 +1000

    Automatic Testing Version

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,8 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000',  // 确保 baseUrl 是正确的
-    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',  // 默认测试文件路径模式
-    supportFile: false,  // 如果不使用支持文件，可以设置为false
+    baseUrl: 'http://localhost:3000',
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    supportFile: false,
   },
 });
--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
     e2e: {
         baseUrl: 'http://localhost:3000',
-        supportFile: false, // 禁用 supportFile
+        supportFile: false,
         setupNodeEvents(on, config) {
             // implement node event listeners here
         },
