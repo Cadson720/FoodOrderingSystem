dc203968b3f1acbec13b0c420c31c062bdb7736d
Raymond <86973702+RaymondCao001@users.noreply.github.com>
2024-09-17 16:34:32 +1000

    1

--- cypress.config.js
+++ cypress.config.js
@@ -2,10 +2,8 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000',
-    supportFile: false, // 禁用 supportFile
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: 'http://localhost:3000',  // 确保 baseUrl 是正确的
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',  // 默认测试文件路径模式
+    supportFile: false,  // 如果不使用支持文件，可以设置为false
   },
 });
