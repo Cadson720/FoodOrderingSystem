a7b6e3e30f5bea7e20477599532becc02de1edc2
Raymond <86973702+RaymondCao001@users.noreply.github.com>
2024-09-19 22:28:27 +1000

    Test Again

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
-    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    specPattern: 'cypress/e2e/**/*.spec.{js,jsx,ts,tsx}',
     supportFile: false,
   },
 });
