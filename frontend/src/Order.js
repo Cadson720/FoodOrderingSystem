ad6c6610533c5970b968ab597a847409ce7fb93f
Shadowfish123 <160587578+Shadowfish123@users.noreply.github.com>
2024-10-03 21:00:29 +1000

    Add files via upload
    add order and order details in frontend

---
+++ frontend/src/Order.js
@@ -0,0 +1,95 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function Order() {
+
+  const [search, setSearch] = useState({});
+  const [orders, setOrders] = useState([]);
+
+  const fetchOrders = () => {
+    axios.get('http://localhost:3001/api/orderlist')
+      .then(response => {
+        setOrders(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching orders:', error);
+      });
+  }
+
+  const handleDelete = (id) => {
+    axios.delete(`http://localhost:3001/api/order/${id}`)
+      .then(response => {
+        fetchOrders();
+      })
+      .catch(error => {
+        console.error('Error deleting order:', error);
+      });
+  }
+
+  const handleInputChange = (e) => {
+    setSearch({ ...search, [e.target.name]: e.target.value });
+  };
+
+  const handleSearch = () => {
+    axios.get('http://localhost:3001/api/orderlist')
+      .then(response => {
+        setOrders(response.data.filter(order => order.order_id == search.orderId || order.create_date == search.createDate));
+      })
+      .catch(error => {
+        console.error('Error fetching orders:', error);
+      });
+  };
+
+  useEffect(() => {
+    fetchOrders();
+  }, []);
+
+  return (
+    <div className="inventory-container">
+      <h2>Order</h2>
+      <div>
+        <label>Date:</label>
+        <input type="date" name="createDate" placeholder='Date' value={search.date} onChange={handleInputChange}/>
+        <label>Id:</label>
+        <input type="text" name="orderId" placeholder='OrderId' value={search.orderId} onChange={handleInputChange}/>
+        <button onClick={handleSearch}>Search</button>
+      </div>
+      <table className="inventory-table">
+        <thead>
+        <tr>
+            <th>Order Id</th>
+            <th>Menu Id</th>
+            <th>Quantity</th>
+            <th>createDate</th>
+            <th>status</th>
+            <th>View</th>
+            <th>Modify</th>
+            <th>Delete</th>
+        </tr>
+        </thead>
+        <tbody>
+          {
+            orders.map(order => (
+              <tr key={order.order_id}>
+                <td>{order.order_id}</td>
+                <td>{order.menu_id}</td>
+                <td>{order.quantity}</td>
+                <td>{order.createDate}</td>
+                <td>{order.status}</td>
+                <td>
+                   <a href={`/orderdetail/${order.order_id}/view`}>View</a>
+                </td>
+                <td>
+                  {order.status === 'pending' ? <a href={`/orderdetail/${order.order_id}/edit`}>Edit</a> : ''}
+                </td>
+                <td>{order.status === 'pending' ? <button onClick={() => handleDelete(order.order_id)}>Delete</button> : ''}</td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default Order;
---
+++ frontend/src/OrderDetail.js
@@ -0,0 +1,94 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+
+function OrderDetail() {
+
+  const [orders, setOrders] = useState([]);
+
+  const { orderId, method } = useParams();  // Get the item ID from the URL
+
+  const fetchOrders = () => {
+    axios.get(`http://localhost:3001/api/orderdetail/${orderId}`)
+      .then(response => {
+        setOrders(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching orders:', error);
+      });
+  }
+
+  const handleInputChange = (e, order) => {
+    setOrders(orders.map(o => {
+      if (o.order_id === order.order_id) {
+        return { ...order, [e.target.name]: e.target.value };
+      }
+      return o;
+    }));
+  }
+
+  const handleSubmit = () => {
+    axios.put(`http://localhost:3001/api/orderdetail/${orderId}`, {orders})
+      .then(response => {
+        console.log(response.data);
+        alert('Order updated successfully');
+      })
+      .catch(error => {
+        console.error('Error updating orders:', error);
+      });
+  };
+
+  useEffect(() => {
+    fetchOrders();
+  }, []);
+
+  return (
+    <div className="inventory-container">
+      <h2>Order</h2>
+      <table className="inventory-table">
+        <thead>
+        <tr>
+            <th>Order Id</th>
+            <th>Price</th>
+            <th>Quantity</th>
+            <th>Subtotal</th>
+            <th>Shipping address</th>
+            <th>Instructions</th>
+        </tr>
+        </thead>
+        <tbody>
+          {
+            orders.map(order => (
+              <tr key={order.order_id}>
+                <td>{order.order_id}</td>
+                <td>{order.price}</td>
+                <td>{order.quantity}</td>
+                <td>{order.subtotal}</td>
+                {
+                  method === 'edit' ? (
+                    <>
+                      <td><input type="text" name="shipping_address" value={order.shipping_address} onChange={(e) => handleInputChange(e, order)} /></td>
+                      <td><input type="text" name="instructions" value={order.instructions} onChange={(e) => handleInputChange(e, order)} /></td>
+                    </>
+                  ) : (
+                    <>
+                      <td>{order.shipping_address}</td>
+                      <td>{order.instructions}</td>
+                    </>
+                  )
+                }
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
+      <div style={{ display: "flex" }}>
+        Total: {orders.reduce((acc, order) => acc + order.subtotal, 0)} &nbsp;
+        {method === 'edit' && <button onClick={handleSubmit}>Submit</button>} &nbsp;
+        <a href='/orders'>Back</a>
+      </div>
+    </div>
+  )
+}
+
+export default OrderDetail;
