ce313cb0fd93ae6b01c431e8cc45ac1873a12153
Cadson <ecadden1@outlook.com>
2024-10-07 21:15:53 +1100

    Implement menu test cases and changed payment.spec.js to paymet.cy.js

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -2,10 +2,11 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
     e2e: {
-        baseUrl: 'http://localhost:3000',
+        baseUrl: 'http://localhost:3000', // Ensure this is the correct base URL
+        specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}', // This ensures Cypress looks for test files in the e2e folder
         supportFile: false,
         setupNodeEvents(on, config) {
-            // implement node event listeners here
+            // implement node event listeners here if needed
         },
     },
 });
